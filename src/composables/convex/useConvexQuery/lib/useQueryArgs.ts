af2aff5204abb111bc416d2bea2b53ed954ccbb1
mickoymouse <mckslvstr@gmail.com>
2025-11-09 16:07:27 +0800

    Add local useConvexQuery to fix some reactive issue.

---
+++ src/composables/convex/useConvexQuery/lib/useQueryArgs.ts
@@ -0,0 +1,17 @@
+import type { OptionalRestArgsAndOptions } from "../../optional.types";
+import type { FunctionReference } from "convex/server";
+import type { UseConvexQueryOptions } from "../../queryReturn.types";
+import { computed, toValue } from "vue";
+
+/**
+ * A composable that returns the query arguments and options for use in Convex queries.
+ * It determines whether the query arguments are optional and provides a computed reference to the arguments and options.
+ */
+export function useQueryArgs<Query extends FunctionReference<"query">>(
+  rest: OptionalRestArgsAndOptions<Query, UseConvexQueryOptions>,
+) {
+  const args = computed(() => toValue(rest[0]));
+  const options = rest[1];
+
+  return { args, options };
+}

7ef5b5c0ce9dba788d775e35610ab24593f45073
mickoymouse <30593627+mickoymouse@users.noreply.github.com>
2025-11-11 23:14:38 +0800

    Add vue toastification

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import { convexVue } from "convex-vue";
 import { createPinia } from "pinia";
+import Toast, { POSITION, type PluginOptions, TYPE } from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 import App from "./App.vue";
 import router from "./router";
@@ -11,5 +13,22 @@ app.use(convexVue, { url: import.meta.env.VITE_CONVEX_URL });
 
 app.use(router);
 app.use(pinia);
+
+const toastOptions: PluginOptions = {
+  position: POSITION.BOTTOM_RIGHT,
+  maxToasts: 3,
+  toastDefaults: {
+    // Here we define per-type defaults
+    success: {
+      toastClassName: "bg-success-toast",
+    },
+    error: {
+      bodyClassName: "font-sans text-sm",
+    },
+  },
+  closeButton: false,
+};
+
+app.use(Toast, toastOptions);
 
 app.mount("#app");

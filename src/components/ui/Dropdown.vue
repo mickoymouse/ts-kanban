3db63a8697f7fa1dcf160143d88f32c1a52e3dcb
mickoymouse <mckslvstr@gmail.com>
2025-11-21 14:49:45 +0800

    Add scroll on dropdown component.

--- src/components/ui/Dropdown.vue
+++ src/components/ui/Dropdown.vue
@@ -70,7 +70,7 @@ onUnmounted(() => {
     </button>
     <div
       v-show="open"
-      class="absolute z-10 bg-(--cst-bg2) rounded-b-md"
+      class="absolute z-10 bg-(--cst-bg2) rounded-b-md max-h-[200px] overflow-auto scrollbar-hide"
       :style="{ width: buttonWidth }"
     >
       <ul>
